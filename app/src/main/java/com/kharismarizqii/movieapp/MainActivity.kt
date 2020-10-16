1aa4c9a480abf25ac9dcc5218f6711ddf80c0806
kharismarizqii <rizqikharisma@gmail.com>
2020-10-16 23:44:53 +0700

    implement details

--- app/src/main/java/com/kharismarizqii/movieapp/MainActivity.kt
+++ app/src/main/java/com/kharismarizqii/movieapp/MainActivity.kt
@@ -32,4 +32,8 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 }
--- app/src/main/java/com/kharismarizqii/movieapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/kharismarizqii/movieapp/ui/details/DetailsFragment.kt
@@ -1,8 +1,61 @@
 package com.kharismarizqii.movieapp.ui.details
 
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.kharismarizqii.movieapp.R
+import com.kharismarizqii.movieapp.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment(R.layout.fragment_details){
+    private val args by navArgs<DetailsFragmentArgs>()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentDetailsBinding.bind(view)
+
+        binding.apply {
+            val movie = args.movie
+            Glide.with(this@DetailsFragment)
+                .load("${movie.baseUrl}${movie.poster_path}")
+                .error(R.drawable.ic_error)
+                .listener(object : RequestListener<Drawable>{
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.isVisible = false
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.isVisible = false
+                        tvDescription.isVisible = true
+                        tvMovieTitle.isVisible = true
+                        return false
+                    }
+
+                })
+                .into(ivMoviePoster)
+
+            tvDescription.text = movie.overview
+            tvMovieTitle.text = movie.original_title
+        }
+    }
 }
